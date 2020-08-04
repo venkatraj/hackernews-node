199bb740b8c556b5695d72f365e09c62bdf9b8af
Venkat <venkat@codinggeek.com>
2020-08-04 11:02:30 +0530

    filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,17 @@ const Query = {
   info: () => {
     return `This is API of hackernews clone`;
   },
-  feed: async (parent, args, ctx) => await ctx.prisma.link.findMany(),
+  feed: async (parent, args, ctx) => {
+    const where = args.filter
+      ? {
+          OR: [
+            { description: { contains: args.filter } },
+            { url: { contains: args.filter } },
+          ],
+        }
+      : {};
+    return await ctx.prisma.link.findMany({ where });
+  },
   users: async (parent, args, ctx) => await ctx.prisma.user.findMany(),
   votes: async (parent, args, ctx) => await ctx.prisma.vote.findMany(),
 };
